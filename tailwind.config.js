997a21209d4e4b997275a0841d4706708ae38545
Esdras Daniel <esdrasdaniel1016@gmail.com>
2026-01-26 23:24:30 -0300

    projeto finalizado

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: "var(--font-inter)",
+      },
+      gridTemplateRows: {
+        app: "min-content max-content",
+      },
+    },
+  },
+  plugins: [],
+};
