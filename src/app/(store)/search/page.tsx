da7283febef154ead2003ec1d5009c52d4a22d1c
Esdras Daniel <esdrasdaniel1016@gmail.com>
2026-01-26 23:17:38 -0300

    projeto finalizado

---
+++ src/app/(store)/search/page.tsx
@@ -0,0 +1,65 @@
+import { api } from "@/data/api";
+import { Product } from "@/data/types/product";
+import formatPrice from "@/utils/formatPrice";
+import Image from "next/image";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+
+interface SearchPageProps {
+  searchParams: Promise<{ q: string }>;
+}
+
+async function searchProducts(query: string): Promise<Product[]> {
+  const response = await api(`/products/search?q=${query}`, {
+    next: {
+      revalidate: 60 * 60,
+    },
+  });
+
+  const products = await response.json();
+
+  return products;
+}
+
+export default async function SearchPage({ searchParams }: SearchPageProps) {
+  const { q: query } = await searchParams;
+
+  if (!query) redirect('/');
+
+  const products = await searchProducts(query);
+
+  return (
+    <div className="flex flex-col gap-4">
+      <p className="text-sm">
+        Resultados para: <span className="font-semibold">{query}</span>
+      </p>
+
+      <div className="grid grid-cols-3 gap-6">
+        {products.map((product) => (
+          <Link
+            key={product.id}
+            href={`/product/${product.slug}`}
+            className="group relative rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
+          >
+            <Image
+              loading="eager"
+              src={product.image}
+              width={400}
+              height={400}
+              quality={100}
+              alt=""
+              className="group-hover:scale-105 transition-transform duration-500"
+            />
+
+            <div className="absolute bottom-28 right-28 h-12 flex items-center gap-2 max-w-700 rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+              <span className="text-sm truncate">{product.title}</span>
+              <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+                {formatPrice(product.price)}
+              </span>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+}
